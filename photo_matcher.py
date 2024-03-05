3ba28cd6252154da556d9c76e991c3a44038565b
initialp00 <73796306+initialp00@users.noreply.github.com>
2024-03-05 04:04:17 +0400

    image extractor

--- photo_matcher.py
+++ photo_matcher.py
@@ -21,22 +21,21 @@ def match_photos_to_system_ids(excel_file, photo_folder, output_file):
         image_filename = f"{system_id}.jpg" 
         if image_filename in image_files:
             photo_path = os.path.join(photo_folder, image_filename)
-            photo_column_index = 11  
+            photo_column_index = 8
             photo_column = new_ws.cell(row=row[0], column=photo_column_index) 
             
             img = Image(photo_path)
             
-            img.width = 500
-            img.height = 500
+            img.width = 5
+            img.height = 5
             
             new_ws.add_image(img, photo_column.coordinate)
     
     new_wb.save(output_file)
 
 # path to excel file, photos folder and output file
-excel_file_path = "employees.xlsx"
-photo_folder_path = "/Photos"
+excel_file_path = "C:/Users/Vipul/Documents/GitHub/BHM/Female_Admin_Leads.xlsx"
+photo_folder_path = "C:/Users/Vipul/Documents/GitHub/BHM/Photos"
 output_file_path = "employees_with_photos.xlsx"
-
 #calling the function to host file, host folder, output file
 match_photos_to_system_ids(excel_file_path, photo_folder_path, output_file_path)
